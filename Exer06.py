d88042f8ed7684db52bec80727db478790f18c5e
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-09-23 10:27:44 -0400

    Exercicio 6:

--- Exer06.py
+++ Exer06.py
@@ -7,5 +7,11 @@ print('{:-^40}'.format(' Exercicio 6 '))
 numero = int(input('digite um numero: '))
 
 d = numero * 2
-t
-r
+t = numero * 3
+r = numero ** (1/2)
+
+print(f'o doblo do numero: {d}')
+print(f'o triplo do numero: {t}')
+print(f'a raiz quadrada do numero: {r}')
+
+print('{:-^40}'.format(''))
