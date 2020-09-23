5929489c17ba4e1d4b8355549c533a28cc868708
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-09-23 10:22:53 -0400

    Exercicio 7: calcula a media de duas provas

--- Exer06.py
+++ Exer06.py
@@ -7,9 +7,5 @@ print('{:-^40}'.format(' Exercicio 6 '))
 numero = int(input('digite um numero: '))
 
 d = numero * 2
-t = numero * 3
-r = numero ** (1/2)
-
-print(f'o dobro do numero: {d}\no triplo do numero: {t}\na raiz quadrada: {r}')
-
-print('{:-^40}'.format(''))
+t
+r
---
+++ Exer07.py
@@ -0,0 +1,14 @@
+"""
+ algoritmo que calcula a media de um aluno com duas notas
+"""
+
+print('{:-^40}'.format(' Exercicio 7 '))
+
+n1 = int(input('informe a primeira nota da prova: '))
+n2 = int(input('informe a segunda nota da prova: '))
+
+m = (n1 + n2) / 2
+
+print(f'a media é {m}')
+
+print('{:-^40}'.format(''))
