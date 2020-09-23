e46e851966f41f2033eadd718c2d170400d82cc1
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-09-23 11:10:15 -0400

    Exercicio 8:

--- Exer09.py
+++ Exer09.py
@@ -4,6 +4,7 @@ Tabuada
 
 tabuada = int(input('informe qual tabuada: '))
 
+print('='*20)
 print(f'{tabuada} X {1} = {tabuada*1}')
 print(f'{tabuada} X {2} = {tabuada*2}')
 print(f'{tabuada} X {3} = {tabuada*3}')
@@ -14,3 +15,4 @@ print(f'{tabuada} X {7} = {tabuada*7}')
 print(f'{tabuada} X {8} = {tabuada*8}')
 print(f'{tabuada} X {9} = {tabuada*9}')
 print(f'{tabuada} X {10} = {tabuada*10}')
+print('='*20)
