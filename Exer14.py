2572cf8e976002d71e0febea0a46b09ce2916435
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-09-24 22:56:58 -0400

    Exercicio 14: faz a conversao entre graus celsis para fahrenheit

---
+++ Exer14.py
@@ -0,0 +1,9 @@
+"""
+algoritmo que calcula graus 
+"""
+
+celsis = float(input('informe quantos graus celsis: '))
+
+f = ((celsis * 9) / 5) + 32
+
+print('a conversao para fahrenheit {}°F'.format(f))
