bc0fd8dc939ebc2ca8fba00980cd9463a7119ab6
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-10-03 17:05:38 -0400

    Exercicio 16: funcao math.trunc

--- Exer16.py
+++ Exer16.py
@@ -2,8 +2,8 @@
     faz arendodamento usando a funcao math.ceil
 """
 
-from math import ceil
+from math import trunc
 
 numero = float(input('digite um numero: '))
 
-print('o arrendondamento do numero é: {}'.format(ceil(numero)))
+print('o arrendondamento do numero é: {}'.format(trunc(numero)))
