68d8ebfcc44ea16d7e622129047e6bc858eeeca9
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-10-03 16:33:03 -0400

    Exercicio 16: funcao math.ceil

---
+++ Exer16.py
@@ -0,0 +1,9 @@
+"""
+    faz arendodamento usando a funcao math.ceil
+"""
+
+from math import ceil
+
+numero = float(input('digite um numero: '))
+
+print('o arrendondamento do numero é: {}'.format(ceil(numero)))
