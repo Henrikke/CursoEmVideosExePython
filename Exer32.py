60265efed1ec2d7e8e160f40c76dea9bf9306cde
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-10-08 21:20:43 -0400

    Exercicio 32:  algoritmo que calcula se o ano e bissexto ou nao

---
+++ Exer32.py
@@ -0,0 +1,15 @@
+"""
+    algoritmo que calcula se o ano e bissexto ou nao
+"""
+
+from datetime import date
+
+ano = int(input('informe um ano ou digite 0 para pegar o ano atual: '))
+
+if ano == 0:
+    ano = date.today().year
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('o ano {} é bissexto'.format(ano))
+else:
+    print('o ano {} nao é bissexto'.format(ano))
