6f7de04b9e5ac6c03dfbe0213b03bf5a875e699b
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-10-09 21:49:28 -0400

    Cores no Terminal

---
+++ CoresTerminal.py
@@ -0,0 +1,34 @@
+"""
+    algoritmo que adiciona cores no terminal
+    branco, vermelho, verde, amarelo, azul, roxo, cyan, cinza
+
+    \033[m
+"""
+# Codigo de Cores
+
+print('\033[30m Cor Branco')
+print('\033[31m Cor Vermelho')
+print('\033[32m Cor Verde')
+print('\033[33m Cor Amarelo')
+print('\033[34m Cor Azul')
+print('\033[35m Cor Roxo')
+print('\033[36m Cor Cyan')
+print('\033[37m Cor Cinza')
+
+# Codigo de Cores de Fundo
+
+print('\033[40m Cor Branco')
+print('\033[41m Cor Vermelho')
+print('\033[42m Cor Verde')
+print('\033[43m Cor Amarelo')
+print('\033[44m Cor Azul')
+print('\033[45m Cor Roxo')
+print('\033[46m Cor Cyan')
+print('\033[47m Cor Cinza')
+
+# Estilos de Fontes
+
+print('\033[0m Padrao')
+print('\033[1m Bold')
+print('\033[4m Sublinhado')
+print('\033[7m Invertido')
