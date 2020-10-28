d22da7a3346456d6cfab043ded90968961da91bd
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-10-27 22:18:23 -0400

    Exercicio 61:  progresao aritmetica com python

--- Exer62.py
+++ Exer62.py
@@ -7,8 +7,8 @@ razao = int(input('razao da pa: '))
 termo = primeiro
 cont = 1
 while
-while cont <= 10:
-    print(f' {termo} -> ', end='')
-    cont += 1
-    termo += razao
-print('fim')
+    while cont <= 10:
+        print(f' {termo} -> ', end='')
+        cont += 1
+        termo += razao
+    print('fim')
