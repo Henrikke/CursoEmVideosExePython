092aa0c3d179d223d18c900170e3b6f3d94552dc
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-10-27 21:39:48 -0400

    Exercicio 60:  algoritmo que calcula o fatorial

---
+++ Exer60.py
@@ -0,0 +1,8 @@
+"""
+    algoritmo que calcula o fatorial
+"""
+
+from math import factorial
+
+valor = int(input('informe um numero: '))
+print('o fatorial do numero {} é {}'.format(valor, factorial(valor)))
