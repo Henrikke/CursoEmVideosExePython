caba549ac4514de00aff9baae269f080a245f4e7
Henrikke <61205636+Henrikke@users.noreply.github.com>
2020-10-27 22:17:44 -0400

    Exercicio 61:  progresao aritmetica com python

--- Exer61.py
+++ Exer61.py
@@ -1,14 +1,18 @@
 """
-    progresao aritmetica com python
+    progresao aritmetica editada
 """
 primeiro = int(input('primeiro termo: '))
 razao = int(input('razao da pa: '))
 
 termo = primeiro
 cont = 1
-
-while cont <= 10:
-    print(f' {termo} -> ', end='')
-    cont += 1
-    termo += razao
-print('fim')
+mais = 10
+total = 0
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print(f' {termo} -> ', end='')
+        cont += 1
+        termo += razao
+    mais = int(input('\nquantos termos vai querer mais ou digite 0 para cancelar: '))
+print('fim da pa')
